481aa56e614ef86c3ebc416ad31924a54319281c
joseph.xiang <joseph.xiang@evg.co>
2024-12-24 15:32:35 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # langchain-openAI
+
+## Install
+```bash
+pip install -qU langchain-openai
+```
+
+## Usage
---
+++ main.py
@@ -0,0 +1,9 @@
+import getpass
+import os
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o-mini")
